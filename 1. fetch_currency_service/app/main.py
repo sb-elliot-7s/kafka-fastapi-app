883f6653e22731895ceb658d205158c84a679fc7
sb-elliot-7s <s.babichevv91p@gmail.com>
2022-05-07 19:37:33 +0300

    fix

--- 1. fetch_currency_service/app/main.py
+++ 1. fetch_currency_service/app/main.py
@@ -1,6 +1,5 @@
 import json
 import uuid
-
 import faust
 from currency_data_service import CurrencyDataProvider
 from configs import get_configs
@@ -10,16 +9,15 @@ app = faust.App('currency_app', broker='kafka://localhost')
 raw_currency_data_topic = app.topic('raw_currency_data_topic')
 
 
-@app.timer(interval=10)
+@app.timer(interval=60)
 async def fetch_currencies():
     currency_provider = CurrencyDataProvider(base_url=get_configs().api_url)
     params = {'function': 'CURRENCY_EXCHANGE_RATE', 'apikey': get_configs().api_key, 'from_currency': 'USD', 'to_currency': 'JPY'}
     currency = await currency_provider.fetch_data(params=params)
-
     if currency:
         await raw_currency_data_topic.send(key=uuid.uuid4().bytes, value=json.dumps(currency).encode())
 
 
 @app.task
 async def start():
-    print('start')
+    print('starting')
